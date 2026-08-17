35c784a9abf282c3d4cd9c1913fb1e8d8bf8a697
TemmieDev <apbatch@gmail.com>
2026-08-17 16:16:19 +1000

    draw trees onto map

--- source/ForestSimulation.java
+++ source/ForestSimulation.java
@@ -1,9 +1,21 @@
+import java.awt.Color;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
 
 public class ForestSimulation{
 
     public static void main(String[] args){
+        Tree tree = new Tree(50, 50, 20);
         JFrame frame = new JFrame("Test");
+        JPanel panel = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                tree.draw(g);
+            }
+        };
+        frame.add(panel);
         frame.setSize(500,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
--- source/Tree.java
+++ source/Tree.java
@@ -1,7 +1,31 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 //Creating the states for tree
 public class Tree {
     private String state;
     public boolean isBurning;
+    private int x;
+    private int y;
+    private int size;
+
+    public Tree(int x, int y, int size){
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        state = "GREEN"; //green
+    }
+
+    public void draw(Graphics g){
+        if(state.equals("GREEN")){
+            g.setColor(Color.GREEN);
+        } else if(state.equals("BURNING")){
+            g.setColor(Color.RED);
+        } else if(state.equals("BURNED")){
+            g.setColor(Color.BLACK);
+        }
+        g.fillRect(x, y, size, size);
+    }
 
     public Tree (){
         state = "GREEN"; //green
